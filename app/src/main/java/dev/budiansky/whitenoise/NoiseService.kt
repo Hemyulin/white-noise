15106393d5d3e9de67e6c836787f19f8d8eaf2dc
Daniel Budiansky <daniel.budiansky@protonmail.com>
2026-09-20 20:46:27 +0200

    feat: stop button

---
+++ app/src/main/java/dev/budiansky/whitenoise/NoiseService.kt
@@ -0,0 +1,53 @@
+package dev.budiansky.whitenoise
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import androidx.core.app.NotificationCompat
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+import android.os.Build
+
+
+class NoiseService: Service() {
+    val noiseEngine = NoiseEngine()
+    private val channelId = "noise_playback"
+
+    override fun onCreate() {
+        super.onCreate()
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                channelId,
+                "White noise playback",
+                NotificationManager.IMPORTANCE_LOW
+            )
+
+            val notificationManager =
+                getSystemService(NotificationManager::class.java)
+
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        val notification = NotificationCompat.Builder(this, channelId)
+            .setContentTitle("White Noise")
+            .setContentText("Playing")
+            .setSmallIcon(R.mipmap.ic_launcher)
+            .build()
+
+        startForeground(1, notification)
+
+        when (intent?.action) {
+            "PLAY" -> noiseEngine.start()
+            "STOP" -> noiseEngine.stop()
+        }
+
+        return super.onStartCommand(intent, flags, startId)
+    }
+
+    override fun onBind(intent: Intent?): IBinder? {
+        return null
+    }
+}
